77a9655e749692073989c50e7e6d2c42c8336dec
jslootsky <joshuaslootsky@gmail.com>
2026-03-25 13:34:00 -0400

    implemented keyword search

--- cards/keyword_search.py
+++ cards/keyword_search.py
@@ -1,5 +1,18 @@
 from __future__ import annotations
+from django.db.models import Q
 
 
 def apply_keyword_search(queryset, keyword: str):
+    if not keyword:
+        return queryset
+    
+    #split query into words
+    terms = keyword.strip().split()
+
+    query = Q()
+
+    for term in terms:
+        query &= (
+            Q(title_icontains=term) | Q(summary_icontains=term) | Q(description_icontains=term) | Q(tags_icontains=term) | Q(keyword_icontains=term)
+        )
     return queryset
